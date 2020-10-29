84711232f4171a0b33dbae6559a8d1a7e925962e
Toms <toms.bi@inbox.lv>
2020-10-29 16:24:58 +0000

    Exercise1

---
+++ app/Exercise1/Formatter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exercise1;
+
+class Formatter
+{
+    public static function moneyFormatter(int $money): string
+    {
+        return number_format($money / 100, 2);
+    }
+}
