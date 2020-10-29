231b53c054b7079b0e4e8e1093eedb332df9bf2e
Toms <toms.bi@inbox.lv>
2020-10-29 16:53:50 +0000

    Exercise2

---
+++ app/Exercise2/Point.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Exercise2;
+
+class Point
+{
+    private $x;
+    private $y;
+
+    public function __construct(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function x()
+    {
+        return $this->x;
+    }
+
+    public function y()
+    {
+        return $this->y;
+    }
+
+}
